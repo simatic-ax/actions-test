8d8efcd9c35f22c537faae147162660df3b93986
Felix Krueger <krueger.felix@siemens.com>
2023-01-26 10:46:25 +0100

    Added renovate test

--- renovate-config.js
+++ renovate-config.js
@@ -1,7 +1,8 @@
+const endpoint = process.env.CI_API_V4_URL;
 // fallback to developer token for dry runs
 const token =
   process.env.TOKEN_GITHUB
-const githubToken = process.env.TOKEN_GITHUB;
+const githubToken = process.env.GITHUB_TOKEN;
 const dryRun = process.env.DRY_RUN !== "false";
 const autodiscoverFilter = process.env.AUTODISCOVER_FILTER;
 const apaxNpmrc = process.env.APAX_NPMRC;
@@ -9,40 +10,20 @@ const apaxNpmrc = process.env.APAX_NPMRC;
 const prFooter = `:space_invader: :sparkles: This merge request is proudly presented by [Renovate Bot](https://code.siemens.com/ax/devops/renovate-bot).`;
 
 module.exports = {
-  extends: [
-    ":semanticPrefixFixDepsChoreOthers",
-    "group:allNonMajor",
-    "workarounds:all",
-  ],
+
   platform: "github",
-  platformAutomerge: true,
   gitAuthor: "AX Bot <botax.industry@siemens.com>",
-  dryRun: dryRun,
   prFooter: prFooter,
   allowPostUpgradeCommandTemplating: true,
   allowedPostUpgradeCommands: [".+"],
-  assigneesFromCodeOwners: true,
-  assigneesSampleSize: 1,
   autodiscover: !!autodiscoverFilter,
   autodiscoverFilter: autodiscoverFilter,
-  ignorePaths: [
-    "**/node_modules/**",
-    "**/bower_components/**",
-    "**/vendor/**",
-    "**/examples/**",
-    "**/__tests__/**",
-    "**/__fixtures__/**",
-  ],
-  rangeStrategy: "pin",
-  pinDigests: true,
   logFile: process.env.LOG_FILE,
   logFileLevel: process.env.LOG_FILE_LEVEL || "debug",
   cacheDir: process.env.CACHE_DIR,
-  dependencyDashboard: true,
   allowScripts: true,
   exposeAllEnv: true,
   ignoreScripts: true,
-  filterUnavailableUsers: true,
   npmrc: process.env.NPMRC,
   labels: ["renovate", `renovate-v${process.env.VERSION}`],
   regexManagers: [
@@ -66,65 +47,8 @@ module.exports = {
         String.raw`#\s*renovate:\s+datasource=(?<datasource>.*?)\s+depName=(?<depName>[\.\w]+)[\s-]+[\w]+_VERSION\s*=\s*"?(?<currentValue>[\d\.^\-\w]*)"?`,
       ],      
     },
-    {
-      fileMatch: ["^terragrunt\\.hcl$"],
-      matchStrings: [
-        // We're using `String.raw` here so that the RegEx can be easily copied from/to other tools (e.g. https://regex101.com/)
-        String.raw`#\s*renovate:\s+datasource=(?<datasource>.*?) depName=(?<depName>.*?)\sregistryUrl=(?<registryUrl>.*?)\s.*?_version\s*=\s*"(?<currentValue>.*)"\s`,
-      ],
-      versioningTemplate:
-        "{{#if versioning}}{{{versioning}}}{{else}}semver{{/if}}",
-    },
-    {
-      fileMatch: ["(^|\\/)pyproject\\.toml$"],
-      matchStrings: [
-        // We're using `String.raw` here so that the RegEx can be easily copied from/to other tools (e.g. https://regex101.com/)
-        String.raw`#\s*renovate:\s+datasource=(?<datasource>.*?) depName=(?<depName>.*?)\sregistryUrl=(?<registryUrl>.*?)\s.*?\.git@(?<currentValue>.*)"`,
-      ],
-      versioningTemplate:
-        "{{#if versioning}}{{{versioning}}}{{else}}semver{{/if}}",
-    },
-    {
-      fileMatch: ["^Dockerfile$"],
-      matchStrings: [
-        // We're using `String.raw` here so that the RegEx can be easily copied from/to other tools (e.g. https://regex101.com/)
-        String.raw`datasource=(?<datasource>.*?) depName=(?<depName>.*?)( versioning=(?<versioning>.*?))?\sENV .*?_VERSION=(?<currentValue>.*)\s`,
-      ],
-      versioningTemplate:
-        "{{#if versioning}}{{{versioning}}}{{else}}semver{{/if}}",
-    },
-    {
-      fileMatch: ["^Dockerfile$"],
-      matchStrings: [
-        // We're using `String.raw` here so that the RegEx can be easily copied from/to other tools (e.g. https://regex101.com/)
-        String.raw`ARG IMAGE=(?<depName>.*?):(?<currentValue>.*?)@(?<currentDigest>sha256:[a-f0-9]+)s`,
-      ],
-      datasourceTemplate: "docker",
-    },
-    {
-      fileMatch: [".gitlab-ci.ya?ml$"],
-      matchStrings: [
-        // We're using `String.raw` here so that the RegEx can be easily copied from/to other tools (e.g. https://regex101.com/)
-        String.raw`datasource=(?<datasource>.*?) depName=(?<depName>.*?)( versioning=(?<versioning>.*?))?\s.*?_VERSION:\s*"?(?<currentValue>[^(\"|\n|@)]*)(?:@(?<currentDigest>[^(\"|\n)]*))?"?\s`,
-      ],
-      versioningTemplate:
-        "{{#if versioning}}{{{versioning}}}{{else}}semver{{/if}}",
-    },
   ],
-  hostRules: [
-    {
-      matchHost: "github.com",
-      token: githubToken,
-    },
-  ],
-  // Note that package rules are combined from top to bottom (in case of conflicts later rules take precedence)
-  // See https://docs.renovatebot.com/configuration-options/#packagerules for some more information.
   packageRules: [
-    {
-      // Ensure that SemVer ranges for engines are preserved.
-      matchDepTypes: ["engines"],
-      rangeStrategy: "replace",
-    },
     {
       // Set endpoint and credentials for the Apax registry
       matchPaths: ["**/{test.,test-windows.,}apax.y{a,}ml"],
@@ -151,16 +75,6 @@ module.exports = {
         ],
         fileFilters: ["**/apax-lock.json"],
       },
-    },
-    {
-      // Ignore intermediate versions
-      matchCurrentVersion: "/^0\\.0\\.0-replace-in-pipeline$/",
-      enabled: false,
-    },
-    {
-      // ignore invalid package that IS unlisted and should not reside in artifactory to begin with
-      matchPackagePatterns: ["^system.componentmodel.composition"],
-      allowedVersions: "!/2010.2.11/",
     },
   ],
 };
